7c5eec7115302bc2b1cf98e9dd44999abd5d6b1a
Vidura Adikari <vidura.ce@gmail.com>
2022-03-23 17:54:47 +0530

    package author update

---
+++ rollup.config.js
@@ -0,0 +1,61 @@
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from '@rollup/plugin-node-resolve';
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+// import image from '@rollup/plugin-image';
+// import scss from 'rollup-plugin-scss';
+// import svg from 'rollup-plugin-svg';
+import {terser} from "rollup-plugin-terser";
+
+import pkg from "./package.json";
+
+// @rollup/plugin-typescript @rollup/plugin-commonjs rollup-plugin-peer-deps-external @rollup/plugin-node-resolve
+// rollup-plugin-dts rollup-plugin-terser
+// rollup-plugin-postcss rollup-plugin-scss @rollup/plugin-image rollup-plugin-svg
+
+export default [
+  {
+    input: "src/library.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      // {
+      //   file: pkg.module,
+      //   format: "esm",
+      //   sourcemap: true,
+      // },
+    ],
+    plugins: [
+
+
+      peerDepsExternal(),
+
+      resolve(),
+      commonjs(),
+      typescript({tsconfig: "./tsconfig-rollup.json"}),
+
+      // babel({
+      //   exclude: 'node_modules/**',
+      //   babelHelpers: 'bundled'
+      // }),
+
+      // scss(),
+      // image(),
+      // svg(),
+      postcss(),
+      terser()
+    ],
+    // external: ['react', 'react-dom', 'react-scripts']
+  },
+  {
+    input: "dist/types/library.d.ts",
+    output: [{file: "dist/index.d.ts", format: "esm"}],
+    plugins: [dts()],
+    external: [/\.css$/],
+  },
+];
